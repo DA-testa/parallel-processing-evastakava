1e8ff94de74e029cfd56404b9db53e4647c08145
evastakava <125592045+evastakava@users.noreply.github.com>
2023-03-19 08:17:28 +0000

    test

--- main.py
+++ main.py
@@ -3,22 +3,22 @@ import heapq
 
 def parallel_processing(n, m, data):
     output = []
-    threads = [(0, i) for i in range(n)]
-    heapq.heapify(threads)
+    th = [(0, i) for i in range(n)]
+    heapq.heapify(th)
     jobs = [(t, i) for i, t in enumerate(data)]
-    curr_jobs = {}
+    c_jobs = {}
 
     for j in jobs:
         t, i = j
-        thread_start, thread_id = heapq.heappop(threads)
-        output.append((thread_id, thread_start))
-        curr_jobs[thread_id] = i
-        heapq.heappush(threads, (thread_start + t, thread_id))
+        th_start, th_id = heapq.heappop(th)
+        output.append((th_id, th_start))
+        c_jobs[th_id] = i
+        heapq.heappush(th, (th_start + t, th_id))
 
     return output
 
 def main():
-    
+
 
     n, m = map(int, input().split())
     data = list(map(int, input().split()))
